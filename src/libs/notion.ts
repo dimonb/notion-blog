5901b54973f547a94c052ab85876343b20fa3dee
Dmitrii Balabanov <dimonb@gmail.com>
2024-02-02 12:45:40 +0200

    sync

--- src/libs/notion.ts
+++ src/libs/notion.ts
@@ -9,23 +9,32 @@ export function getRecordMap(id: string) {
     return notion.getPage(id);
 }
 
-function findAllString(obj: any) {
+function findAllString(obj: any, level = 0) {
+  if (level > 10) {
+    return [];
+  }
   const result: string[] = [];
   if (typeof obj === 'string') {
     result.push(obj);
   } else if (typeof obj === 'object') {
     for (const key in obj) {
-      result.push(...findAllString(obj[key]));
+      result.push(...findAllString(obj[key], level+1));
     }
   }
   return result;
+}
+
+function isImagePath(path: string): boolean {
+  // Regular expression to match common image file extensions
+  const imagePattern = /\.(jpeg|jpg|gif|png|bmp|svg|webp)$/i;
+  return imagePattern.test(path);
 }
 
 export async function getImageMap(recordMap: any){
   const imageMap = new Map<string, string>();
 
   for (const str of findAllString(recordMap)) {
-    if (str.startsWith('https://')) {
+    if (str.startsWith('https://') && isImagePath(str)) {
       imageMap.set(str, await mapImageUrl(str, recordMap));
     }
   }
